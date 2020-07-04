e4fe066805a3e572915560d2f763a904224fb905
Scott Fitzgerald <sfitzgerald@eero.com>
2020-07-04 14:22:00 -0500

    implement WeeklyPayroll struct and processWeeklyPayroll function

--- week_6/project_6.cpp
+++ week_6/project_6.cpp
@@ -15,17 +15,19 @@
 using namespace std;
 
 // Global constants
-const int NUM_EMPLOYEES = 4;
+const int NUM_EMPLOYEES = 2;
+const double INCOME_TAX_RATE = .15;
 
 // Function prototypes
 struct EmployeeInfo;
+struct WeeklyPayroll;
 EmployeeInfo getEmployeeInfo(int);
 int getIdNumber();
 string getName();
 double getPayRate();
 int getEmpType();
 double getTimecardInfo(const string&);
-
+WeeklyPayroll processWeeklyPayroll(double, double);
 void payrollReport();
 
 struct EmployeeInfo {
@@ -35,9 +37,19 @@ struct EmployeeInfo {
     int empType{};
 };
 
+struct WeeklyPayroll {
+    int empId{};
+    string empName;
+    double grossPay{};
+    double incomeTax{};
+    double netPay{};
+};
+
+
 int main() {
     EmployeeInfo employees[NUM_EMPLOYEES];
     double hoursWorked[4];
+    WeeklyPayroll employeePay[NUM_EMPLOYEES];
 
     cout << "Armadillo Automotive Group payroll program." << endl;
 
@@ -51,6 +63,13 @@ int main() {
     cout << "\nEnter timecard information for each employee: " << endl;
     for (int i = 0; i < NUM_EMPLOYEES; i++) {
         hoursWorked[i] = getTimecardInfo(employees[i].empName);
+    }
+
+    // Process payroll info
+    for (int i = 0; i < NUM_EMPLOYEES; i++) {
+        employeePay[i] = processWeeklyPayroll(hoursWorked[i], employees[i].hourlyPayRate);
+        employeePay[i].empId = employees[i].empId;
+        employeePay[i].empName = employees[i].empName;
     }
 
     cout << endl;
@@ -118,6 +137,28 @@ double getTimecardInfo(const string& empName) {
     cin >> hours;
 
     return hours;
+}
+
+WeeklyPayroll processWeeklyPayroll(double numHours, double payRate) {
+    WeeklyPayroll employeePay;
+    double overtimePay = 0;
+
+    // Calculate gross pay -- overtime is worth 1.5x
+    if (numHours > 40) {
+        overtimePay = (numHours - 40) * 1.5 * payRate;
+        employeePay.grossPay = payRate * 40 + overtimePay;
+    } else {
+        employeePay.grossPay = numHours * payRate;
+    }
+
+    // Calculate tax by multiplying numHours worked by tax rate
+    employeePay.incomeTax = employeePay.grossPay * INCOME_TAX_RATE;
+
+    // Calculate net pay by subtracting tax from gross
+    employeePay.netPay = employeePay.grossPay - employeePay.incomeTax;
+
+    return employeePay;
+
 }
 
 void payrollReport() {
